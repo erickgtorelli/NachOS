c988c3637ab6c238deacb0aaf99ab531c2d35fce
Leandro09 <LuisLeandro09@gmail.com>
2015-06-05 08:28:24 -0600

    Cambios en tabla de hilos

--- nachos64/code/userprog/threadsTabla.cc
+++ nachos64/code/userprog/threadsTabla.cc
@@ -3,12 +3,53 @@ typedef int SpaceId;
 threadsTabla::threadsTabla(){
 	threadsOn = new Thread*[MAX_THREADS];
 	threadsOnMap = new BitMap(MAX_THREADS);
+	hilosJoin = new hilosEnEspera[MAX_THREADS] ;
+	BitMap * hilosMapa = new BitMap(MAX_THREADS);
 }
 
 threadsTabla::~threadsTabla(){
 	delete threadsOn;
 	delete threadsOnMap;
+	delete hilosJoin;
+	delete hilosMapa;
 }
+/*
+int threadsTabla::addJoin(Thread* thread, Semaphore* s,int identificador, SpaceId id){
+	//int identificador = hilosMapa->Find();
+	hilosJoin[identificador].hilo = thread;
+	hilosJoin[identificador].sem = s;
+	hilosJoin[identificador].hiloEsperado = id;
+	//hilosMapa->Mark(identificador);
+	return identificador;
+}
+*/
+/*
+int threadsTabla::delJoin(Thread* thread, Semaphore* s,int identificador, SpaceId id){
+	hilosJoin[identificador].hilo = NULL;
+	hilosJoin[identificador].sem = NULL;
+	hilosJoin[identificador].hiloEsperado = -1;
+	//hilosMapa->Clear(identificador);
+	return identificador;
+
+}*/
+int threadsTabla::addJoin(Thread* thread, Semaphore* s, SpaceId id){
+	int identificador = hilosMapa->Find();
+	hilosJoin[identificador].hilo = thread;
+	hilosJoin[identificador].sem = s;
+	hilosJoin[identificador].hiloEsperado = id;
+	hilosMapa->Mark(identificador);
+	return identificador;
+}
+
+int threadsTabla::delJoin(Thread* thread, Semaphore* s,int identificador, SpaceId id){
+	hilosJoin[identificador].hilo = NULL;
+	hilosJoin[identificador].sem = NULL;
+	hilosJoin[identificador].hiloEsperado = -1;
+	hilosMapa->Clear(identificador);
+	return identificador;
+
+}
+
 
 SpaceId threadsTabla::AddThread(Thread* thread){
 	int clearPosition = threadsOnMap->Find();
